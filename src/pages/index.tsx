cff322cb5cfcbd8887b97a54fe8e9f3e0e3a52e0
Rogerio Vieira <rogeriovbh@gmail.com>
2023-05-21 18:15:18 -0300

    altera estrutura do projeto e fonte utilizada

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Inicio | jojo.news </title>
+      </Head>
+      <h1>Hello world</h1>
+    </>
+
+  )
+}
