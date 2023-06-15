0e6c0dc2cb9c85f9bea5e305e0b3af10b2ada019
Rogerio Vieira <rogeriovbh@gmail.com>
2023-06-14 22:41:42 -0300

    teste

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ export function Header (){
   return(
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="" alt="   "/>
+        <img src="" alt="  "/>
         <nav>
           <a className={styles.active}> Home </a>
           <a>Projetos</a>
